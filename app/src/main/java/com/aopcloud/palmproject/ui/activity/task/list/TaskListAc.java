db2e459c9109d27bdf970e21a7ab3b11b9294723
admin <1321851830@qq.com>
2020-07-10 00:38:53 +0800

    提交代码

---
+++ app/src/main/java/com/aopcloud/palmproject/ui/activity/task/list/TaskListAc.java
@@ -0,0 +1,14 @@
+package com.aopcloud.palmproject.ui.activity.task.list;
+
+import com.aopcloud.base.annotation.Layout;
+import com.aopcloud.base.base.BaseActivity;
+import com.aopcloud.palmproject.R;
+
+@Layout(R.layout.ac_task_list)
+public class TaskListAc extends BaseActivity {
+
+    @Override
+    protected void initView() {
+
+    }
+}
